c2004600a7aa7e33eb27021537d630a8497aaa44
Sagor Biswas <biswassagor927@gmail.com>
2026-03-17 19:33:24 +0600

    Expanded Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,37 +8,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body:
-         Padding(
-           padding: const EdgeInsets.all(10.0),
-           child: GridView.count(
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: [
-              Container(
-                height: 200,
-                width: 200,
-                color: Colors.tealAccent,
-              ),
-               Container(
-                height: 200,
-                width: 200,
-                color: Colors.tealAccent,
-              ),
-               Container(
-                height: 200,
-                width: 200,
-                color: Colors.tealAccent,
-              ),
-               Container(
-                height: 200,
-                width: 200,
-                color: Colors.tealAccent,
-              ),
-            ],
-                   ),
-         ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(color: Colors.tealAccent,),
+            ),
+             Expanded(
+              flex: 1,
+              child: Container(color: Colors.blueGrey,),
+            ),
+             Expanded(
+              flex: 1,
+              child: Container(color: Colors.teal,),
+            ),
+          ],
+        ),
+        
       ),
     );
   }
