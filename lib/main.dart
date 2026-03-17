5ca5fab1baeea4d3385db9ff3578878b20cac70c
Sagor Biswas <biswassagor927@gmail.com>
2026-03-17 20:18:34 +0600

    PageView Controll

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,46 @@
+import 'package:firstapp/page1.dart';
+import 'package:firstapp/page2.dart';
+import 'package:firstapp/page3.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:liquid_swipe/liquid_swipe.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(color: Colors.tealAccent,),
-            ),
-             Expanded(
-              flex: 1,
-              child: Container(color: Colors.blueGrey,),
-            ),
-             Expanded(
-              flex: 1,
-              child: Container(color: Colors.teal,),
-            ),
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          controller: _controller,
+          children: const <Widget>[
+            Page1(),
+            Page2(),
+            Page3(),
           ],
         ),
-        
       ),
     );
   }
---
+++ lib/page2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: const Center(
+        child: Text(
+          'Page 2',
+          style: TextStyle(fontSize: 32, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+      
